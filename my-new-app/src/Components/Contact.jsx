116e4d8a93ac0918d4f278098bce228a0c1814f3
Turkane Yusifzade <your-yusifzadeturkanee@gmail.com>
2024-12-10 17:33:08 +0400

    first commit

---
+++ my-new-app/src/Components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="page">
+      <h1>Əlaqə Məlumatları</h1>
+      <p>Email: yusifzadeturkanee@gmail.com</p>
+      <p>Telefon: +994508529701</p>
+      <p>LinkedIn: <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">Profilim</a></p>
+    </div>
+  );
+}
+
+export default Contact;
